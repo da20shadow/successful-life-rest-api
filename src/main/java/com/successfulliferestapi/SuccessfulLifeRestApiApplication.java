6fc297a482235f09cc70fac26f9cb25f6ca43f03
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-04-07 17:38:14 +0300

    initial commit

---
+++ src/main/java/com/successfulliferestapi/SuccessfulLifeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.successfulliferestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuccessfulLifeRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuccessfulLifeRestApiApplication.class, args);
+    }
+
+}
