1f91c5eba9091168c477a8bd279ad27f27eb6c1d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-04-08 12:50:40 +0300

    Created error and success response message dtos

---
+++ src/main/java/com/successfulliferestapi/Shared/models/dto/ErrorResponseDTO.java
@@ -0,0 +1,12 @@
+package com.successfulliferestapi.Shared.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponseDTO {
+    private String message;
+}
---
+++ src/main/java/com/successfulliferestapi/Shared/models/dto/SuccessResponseDTO.java
@@ -0,0 +1,12 @@
+package com.successfulliferestapi.Shared.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class SuccessResponseDTO {
+    private String message;
+}
