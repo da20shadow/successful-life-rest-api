4ed48ce47ab88127964a40c0bf58da3e7139ccf2
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-04-09 17:17:06 +0300

    added messages

--- src/main/java/com/successfulliferestapi/target/constants/TargetMessages.java
+++ src/main/java/com/successfulliferestapi/target/constants/TargetMessages.java
@@ -7,11 +7,14 @@ public class TargetMessages {
         public static final String DELETE = "Unable to delete target. Please try again.";
         public static final String DUPLICATE_TITLE = "A target with the same title already exists.";
         public static final String NO_CHANGES = "There are no changes to save.";
+        public static final String NOT_FOUND = "Target not found!";
     }
 
     public static class Success {
-        public static final String ADD = "Successfully added a new target.";
-        public static final String UPDATE = "Successfully updated the target.";
-        public static final String DELETE = "Successfully deleted the target.";
+        public static final String ADDED = "Successfully added a new target.";
+        public static final String UPDATED = "Successfully updated the target.";
+        public static final String UPDATED_TITLE = "Successfully updated target title!";
+        public static final String UPDATED_DESCRIPTION = "Successfully updated target description!";
+        public static final String DELETED = "Successfully deleted the target.";
     }
 }
