9211dc1f4f0de1b5362f8d8c24bb7c81bd79b29d
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-04-11 21:02:21 +0300

    Created admin controller and service

--- src/main/java/com/successfulliferestapi/admin/controllers/AdminController.java
+++ src/main/java/com/successfulliferestapi/admin/controllers/AdminController.java
@@ -1,9 +1,32 @@
 package com.successfulliferestapi.Admin.controllers;
 
+import com.successfulliferestapi.Admin.services.AdminService;
+import com.successfulliferestapi.Shared.models.dto.ErrorResponseDTO;
+import com.successfulliferestapi.Shared.models.dto.SuccessResponseDTO;
+import com.successfulliferestapi.User.models.entity.User;
+import com.successfulliferestapi.User.models.enums.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/management/api/v1")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/{userId}/ban")
+    public ResponseEntity<?> banUser(@PathVariable Long userId) {
+        try {
+            return ResponseEntity.ok(adminService.banUser(userId));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO(e.getMessage()));
+        }
+    }
 }
--- src/main/java/com/successfulliferestapi/admin/services/AdminService.java
+++ src/main/java/com/successfulliferestapi/admin/services/AdminService.java
@@ -1,7 +1,30 @@
 package com.successfulliferestapi.Admin.services;
 
+import com.successfulliferestapi.Admin.constants.AdminMessages;
+import com.successfulliferestapi.Shared.models.dto.SuccessResponseDTO;
+import com.successfulliferestapi.User.constants.UserMessages;
+import com.successfulliferestapi.User.exceptions.UserException;
+import com.successfulliferestapi.User.models.entity.User;
+import com.successfulliferestapi.User.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class AdminService {
+
+    private final UserRepository userRepository;
+
+    public SuccessResponseDTO banUser(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isEmpty()) {
+            throw new UserException(UserMessages.Error.NOT_FOUND);
+        }
+        User user = optionalUser.get();
+        user.setBanned(true);
+        userRepository.save(user);
+        return new SuccessResponseDTO(AdminMessages.Success.BANNED_USER);
+    }
 }
