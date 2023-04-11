a1ad23c28d932af2b661e76c16f0782a2c530566
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-04-11 21:02:05 +0300

    added validations

--- src/main/java/com/successfulliferestapi/Task/models/dto/EditTaskDTO.java
+++ src/main/java/com/successfulliferestapi/Task/models/dto/EditTaskDTO.java
@@ -2,6 +2,9 @@ package com.successfulliferestapi.Task.models.dto;
 
 import com.successfulliferestapi.Task.models.enums.TaskPriority;
 import com.successfulliferestapi.Task.models.enums.TaskStatus;
+import com.successfulliferestapi.Task.validations.annotations.ValidTaskPriority;
+import com.successfulliferestapi.Task.validations.annotations.ValidTaskStatus;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,21 +17,26 @@ import lombok.Setter;
 @AllArgsConstructor
 public class EditTaskDTO {
     @Size(min = 2, max = 145, message = "Title must be between 2 and 145 characters")
+    @Pattern(regexp = "^[\\p{L}\\p{N}\\p{P}\\p{Z}]*$", message = "Title can only contain letters, numbers, and characters used in sentences.")
     private String title;
 
-    @Size(max = 1024, message = "Description cannot exceed 1024 characters")
+    @Size(min = 5, max = 1024, message = "Description must be between 5 - 1024 characters long.")
     private String description;
 
-    private TaskStatus status;
+    @ValidTaskStatus
+    private String status;
 
-    private TaskPriority priority;
+    @ValidTaskPriority
+    private String priority;
 
     private boolean urgent;
 
     private boolean important;
 
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$\n", message = "Please, enter valid start date format!")
     private String startDate;
 
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$\n", message = "Please, enter valid due date format!")
     private String dueDate;
 
 }
--- src/main/java/com/successfulliferestapi/task/models/dto/AddTaskDTO.java
+++ src/main/java/com/successfulliferestapi/task/models/dto/AddTaskDTO.java
@@ -1,8 +1,8 @@
 package com.successfulliferestapi.Task.models.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import com.successfulliferestapi.Task.validations.annotations.ValidTaskPriority;
+import com.successfulliferestapi.Task.validations.annotations.ValidTaskStatus;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,22 +14,34 @@ import lombok.Setter;
 @AllArgsConstructor
 public class AddTaskDTO {
 
-    @NotBlank(message = "Title cannot be blank")
-    @Size(min = 5, max = 145, message = "Title must be between 2 and 145 characters")
+    @Size(min = 2, max = 145, message = "Title must be between 2 and 145 characters")
+    @Pattern(regexp = "^[\\p{L}\\p{N}\\p{P}\\p{Z}]*$", message = "Title can only contain letters, numbers, and characters used in sentences.")
     private String title;
 
+    @Size(min = 5, max = 1024, message = "Description must be between 5 - 1024 characters long.")
     private String description;
 
-    @NotNull(message = "Task Status is required!")
+    @NotNull(message = "Task status is required!")
+    @ValidTaskStatus
     private String status;
 
-    @NotNull(message = "Task Priority is required!")
+    @NotNull(message = "Task priority is required!")
+    @ValidTaskPriority
     private String priority;
+
     private boolean urgent = false;
+
     private boolean important = false;
+
     private boolean favorite = false;
-    private Long targetId = null;
+
+    @Positive
+    private Long targetId;
+
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$\n", message = "Please, enter valid start date format!")
     private String startDate;
+
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}$\n", message = "Please, enter valid due date format!")
     private String dueDate;
 
 }
