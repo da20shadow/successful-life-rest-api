dc4c3986b58c8082343dfc11a4c2b5c59d8d8192
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-04-13 21:10:29 +0300

    Implemented admin controller

--- src/main/java/com/successfulliferestapi/admin/controllers/AdminController.java
+++ src/main/java/com/successfulliferestapi/admin/controllers/AdminController.java
@@ -1,18 +1,21 @@
 package com.successfulliferestapi.Admin.controllers;
 
+import com.successfulliferestapi.Admin.constants.SettingsNames;
+import com.successfulliferestapi.Admin.models.dtos.SetAllowRegistrationsDTO;
 import com.successfulliferestapi.Admin.services.AdminService;
+import com.successfulliferestapi.Admin.services.AppSettingsService;
 import com.successfulliferestapi.Shared.models.dto.ErrorResponseDTO;
 import com.successfulliferestapi.Shared.models.dto.SuccessResponseDTO;
 import com.successfulliferestapi.User.models.entity.User;
 import com.successfulliferestapi.User.models.enums.UserRole;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.ErrorResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/management/api/v1")
@@ -20,6 +23,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
 
     private final AdminService adminService;
+    private final AppSettingsService appSettingsService;
+
+    @PatchMapping("/settings/allow-registrations")
+    public ResponseEntity<?> setAllowNewRegistrations(@RequestBody SetAllowRegistrationsDTO request) {
+        try {
+
+            return ResponseEntity.ok(
+                    appSettingsService.updateAppSetting(
+                            SettingsNames.ALLOW_REGISTRATIONS,
+                            request.getAllowRegistrations())
+            );
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO(e.getMessage()));
+        }
+    }
+
+    @GetMapping("/settings")
+    public ResponseEntity<?> getSettings() {
+        try {
+            return ResponseEntity.ok(appSettingsService.getSettings());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO(e.getMessage()));
+        }
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getUsers(@RequestParam(defaultValue = "0") int page,
+                                      @RequestParam(defaultValue = "24") int size) {
+        try {
+            Pageable pageable = PageRequest.of(page, size, Sort.by("id").ascending());
+            return ResponseEntity.ok(adminService.getUsers(pageable));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ErrorResponseDTO(e.getMessage()));
+        }
+    }
 
     @GetMapping("/{userId}/ban")
     public ResponseEntity<?> banUser(@PathVariable Long userId) {
--- src/main/java/com/successfulliferestapi/admin/services/AdminService.java
+++ src/main/java/com/successfulliferestapi/admin/services/AdminService.java
@@ -1,12 +1,18 @@
 package com.successfulliferestapi.Admin.services;
 
 import com.successfulliferestapi.Admin.constants.AdminMessages;
+import com.successfulliferestapi.Admin.models.dtos.SetAllowRegistrationsDTO;
 import com.successfulliferestapi.Shared.models.dto.SuccessResponseDTO;
 import com.successfulliferestapi.User.constants.UserMessages;
 import com.successfulliferestapi.User.exceptions.UserException;
+import com.successfulliferestapi.User.models.dto.UserDTO;
 import com.successfulliferestapi.User.models.entity.User;
+import com.successfulliferestapi.User.models.enums.UserRole;
 import com.successfulliferestapi.User.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,6 +22,7 @@ import java.util.Optional;
 public class AdminService {
 
     private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
     public SuccessResponseDTO banUser(Long userId) {
         Optional<User> optionalUser = userRepository.findById(userId);
@@ -27,4 +34,10 @@ public class AdminService {
         userRepository.save(user);
         return new SuccessResponseDTO(AdminMessages.Success.BANNED_USER);
     }
+
+    public Page<UserDTO> getUsers(Pageable pageable) {
+        Page<User> usersPage = userRepository.findByRole(UserRole.USER, pageable);
+        return usersPage.map(user -> modelMapper.map(user, UserDTO.class));
+    }
+
 }
