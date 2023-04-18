d5be1e0356580212639e8c090a82eac1eee0f34b
da20shadow <92220876+da20shadow@users.noreply.github.com>
2023-04-18 20:00:10 +0300

    added id

--- src/main/java/com/successfulliferestapi/User/models/dto/UserDTO.java
+++ src/main/java/com/successfulliferestapi/User/models/dto/UserDTO.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 @Setter
 @RequiredArgsConstructor
 public class UserDTO {
+    private Long id;
     private String firstName;
     private String email;
     private String username;
